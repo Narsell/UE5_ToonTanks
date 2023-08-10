318af6ca407c01950663e7a9be4e6a29b3de9dad
Daniel <narselldev@gmail.com>
2023-08-09 20:42:10 -0500

    Added child c++ class with a camera boom

---
+++ Source/ToonTanks/Tank.cpp
@@ -0,0 +1,19 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Tank.h"
+#include "GameFramework/SpringArmComponent.h"
+#include "Camera/CameraComponent.h"
+
+ATank::ATank() {
+
+	SpringArm = CreateDefaultSubobject<USpringArmComponent>(TEXT("Spring Arm"));
+	SpringArm->SetupAttachment(RootComponent);
+	SpringArm->TargetArmLength = 600.f;
+	SpringArm->SetRelativeRotation(FRotator(-45.0, 0.0, 0.0));
+
+	Camera = CreateDefaultSubobject< UCameraComponent>(TEXT("Camera"));
+	Camera->SetupAttachment(SpringArm);
+
+}
+
